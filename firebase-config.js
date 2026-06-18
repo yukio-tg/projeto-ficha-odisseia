57390a0406d7a9d1766817bf55e290d1349c1789
yukio-tg <dantagata2018@gmail.com>
2026-06-18 19:31:04 -0300

    organização

--- firebase-config.js
+++ firebase-config.js
@@ -1,13 +1,1 @@
-const firebaseConfig = {
-    apiKey: "AIzaSyD0GKHekJIBroBaQbbKIagcaZ52ljsgT6A",
-    authDomain: "fichas-odisseia.firebaseapp.com",
-    projectId: "fichas-odisseia",
-    storageBucket: "fichas-odisseia.firebasestorage.app",
-    messagingSenderId: "438632062292",
-    appId: "1:438632062292:web:68e9584cf8f5ffcf2e3cc1",
-    measurementId: "G-JRFYC354YB"
-};
-
-firebase.initializeApp(firebaseConfig);
-const db = firebase.firestore();
-const auth = firebase.auth();
+//O documento foi censurado para evitar vazamento de informações sensíveis. Este projeto só estará público por pouco tempo.
